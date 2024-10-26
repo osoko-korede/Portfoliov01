33d428b6b99e7124a9bfb1c802148bd98cf911a4
Korede <osokokoredepeter@gmail.com>
2024-10-26 13:43:16 +0100

    Error 404 page

--- src/Page/NoPage.jsx
+++ src/Page/NoPage.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
 
 const NoPage = () => {
   return (
-    <div>NoPage</div>
+    <div className='w-full flex flex-col justify-center items-center h-[80%] my-32 lg:my-52 p-10'>
+      <h1 className='p-5 pb-4 text-7xl md:text-9xl text-center font-semibold tracking-tight green inline-block text-transparent bg-clip-text'>Error 404</h1>
+      <p className='text-center'>This page does not exist. Go back to <NavLink to="/" className="underline">Home</NavLink>.</p>
+    </div>
   )
 }
 
